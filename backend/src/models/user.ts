3ce730701188d5db2ee111a948b5207623e59096
alekigsm <Igr0k@yandex.ru>
2025-12-30 00:30:32 +0300

    завтра

---
+++ backend/src/models/user.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+export interface IToken{
+    token: string;
+}
+
+export interface IUser{
+  name : string;
+  email : string;
+  password : string;
+  tokens : IToken[]
+}
+
+const imageSchema = new mongoose.Schema<IImage>({
+  fileName: {
+    type: String,
+    required: [true, 'Поле "fileName" должно быть заполнено'],
+  },
+  originalName: {
+    type: String,
+    required: [true, 'Поле "originalName" должно быть заполнено'],
+  },
+});
+
+const productSchema = new mongoose.Schema<IProduct>({
+  title: {
+    type: String,
+    required: [true, 'Поле "title" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "title" - 2'],
+    maxlength: [30, 'Максимальная длина поля "title" - 30'],
+    unique: true,
+  },
+  image: {
+    type: imageSchema,
+    required: [true, 'Поле "image" должно быть заполнено'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Поле "category" должно быть заполнено'],
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    default: null,
+  },
+}, {
+  versionKey: false,
+});
+
+export default mongoose.model<IProduct>('product', productSchema);
